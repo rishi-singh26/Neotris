06f97b75c67e51b5b99f96631515e1395a83c39d
rishi-singh26 <rishisingh0831@gmail.com>
2025-03-02 14:18:35 +0530

    Initial Commit

---
+++ Neotris/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Neotris
+//
+//  Created by Rishi Singh on 02/03/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NeotrisTests/NeotrisTests.swift
@@ -0,0 +1,16 @@
+//
+//  NeotrisTests.swift
+//  NeotrisTests
+//
+//  Created by Rishi Singh on 02/03/25.
+//
+
+import Testing
+
+struct NeotrisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
